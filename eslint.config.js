88b217804a34c7cfed97e93855da880d2819a134
psi <cy.lonsido@gmail.com>
2026-06-04 23:43:31 +0800

    chore: ignore vite config in lint

--- eslint.config.js
+++ eslint.config.js
@@ -6,7 +6,7 @@ import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['dist', 'coverage', 'docs']),
+  globalIgnores(['dist', 'coverage', 'docs', 'vite.config.ts']),
   {
     files: ['**/*.{ts,tsx}'],
     extends: [
